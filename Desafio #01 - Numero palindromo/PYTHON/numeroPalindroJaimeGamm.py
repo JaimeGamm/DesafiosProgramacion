23974b8c6b19554bd27f0727b6bbae796d1353dc
jaimegamm <jaimegamm@gmail.com>
2024-04-11 21:21:22 -0500

    cambio

--- Desafio #01 - Numero palindromo/PYTHON/numeroPalindroJaimeGamm.py
+++ Desafio #01 - Numero palindromo/PYTHON/numeroPalindroJaimeGamm.py
@@ -1,9 +1,13 @@
 def es_palindromo(numero):
     # Convierte el número a cadena para facilitar la manipulación de caracteres
+
     str_numero = str(numero)
     if(str_numero == str_numero[::-1]):
+
         print(f" El {numero} es un palíndromo")
+
     else:
+
          print(f" El {numero} no es un palíndromo")
 
 es_palindromo("23322")  
